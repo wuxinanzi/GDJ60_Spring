e600d6a815416969602352d58f80419f59b9665c
miri <miri6507@naver.com>
2023-03-03 11:20:12 +0900

    Commit

---
+++ src/main/webapp/htmljquery/jsStudy_window.js
@@ -0,0 +1,20 @@
+const btn1 = document.getElementById('btn1');
+
+
+btn1.addEventListener('click', function(){
+    //window.open("./new.html", 'test', 'width=200,height=100, top=100, left =200');
+    clearInterval(st);
+})
+
+
+//setTimeout
+
+
+setTimeout(function(){
+    alert('timeout');
+}, 1000);
+
+
+let st = setInterval(function(){
+    console.log('Interval')
+}, 1000)
